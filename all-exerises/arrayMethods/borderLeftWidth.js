14951d3801966425f86e0d43c63996a8a973fa7c
khaikaito <210374351+KAITAKU88@users.noreply.github.com>
2025-07-16 17:34:46 +0700

    Edit arrayMethods

---
+++ all-exerises/arrayMethods/borderLeftWidth.js
@@ -0,0 +1,20 @@
+// Write the function camelize(str) that changes dash-separated words like “my-short-string” into camel-cased “myShortString”.
+// That is: removes all dashes, each word after dash becomes uppercased.
+// Examples:
+// camelize("background-color") == 'backgroundColor';
+// camelize("list-style-image") == 'listStyleImage';
+// camelize("-webkit-transition") == 'WebkitTransition';
+
+
+//pseudocode:
+/*
+- Viết 1 hàm gọi là strToArr(str). Hàm này nhận vào 1 chuỗi, sau đó tách chuỗi này thành các chuỗi nhỏ, sử dụng phân cách là khoảng trắng hoặc dấu gạch ngang (nâng cao 1 chút là bất kỳ ký tự lạ nào như _, -, *, !...). Hàm này trả về 1 mảng các chuỗi nhỏ 
+- Sau đó viết hàm camelize(str), hàm này sẽ gọi hàm strToArr(str), sau đó dùng phương thức nối các phần tử của mảng lại thành 1 chuỗi duy nhất.
+- 
+*/
+
+
+
+//BẮT ĐẦU VIẾT MÃ THẬT
+
+
