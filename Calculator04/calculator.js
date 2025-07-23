fef2b895039de2439ef50a6c720ac733428e5c2d
khaikaito <210374351+KAITAKU88@users.noreply.github.com>
2025-07-23 09:52:42 +0700

    Do round_0, round_2 addEventListener

--- Calculator04/calculator.js
+++ Calculator04/calculator.js
@@ -1,25 +1,34 @@
 // nhánh này sẽ phân tích và viết lại logic từ đầu
+//trả về các phím số 
 let numsBtn = document.querySelectorAll(".number");
 let dot = document.querySelector(".dot");
 let equa = document.querySelector(".equa");
 let pi = document.querySelector(".pi");
 let screen = document.querySelector(".screen");
+//Trả về các toán tử cần 2 toán hạng 
+let opes = document.querySelectorAll(".operator");
+//các toán tử cần 1 toán hạng
 let square = document.querySelector(".square");
 let sqrt = document.querySelector(".sqrt");
+let plusMinus = document.querySelector(".plusMinus");
+let round_0 = document.querySelector(".round_0");
+let round_2 = document.querySelector(".round_2");
+let mc = document.querySelector(".mc");
+let mr = document.querySelector(".mr");
+let mPlus = document.querySelector(".mPlus");
+let mMinus = document.querySelector(".mMinus");
 
 
 
-let operator = "";
+let memoVar = 0;//biến độc lập cho mc, mr, m+, m-
+
+
+
+let operators = "";
 let num1 = 0;
 let num2 = 0; //đây là 2 toán hạng trong phép tính toán 
 
 
-let opes = document.querySelectorAll(".operator");
-
-let forOneOperand = {
-    
-
-}
 
 //object mapping để chuyển từ operator string sang function 
 let operatorToFunction = {
@@ -30,15 +39,10 @@ let operatorToFunction = {
     "pi" : () => {return Math.PI},
     "sqrt" : (a) => {return Math.sqrt(a)},
     "square" : (a) => {return Math.pow(a, 2)},
-    "+-" : (a) => {return (-1)*a},
+    "plusMinus" : (a) => {return (-1)*a},
+    "r2" : (a) => {return Number(a).toFixed(2)},  //hàm này là gì ?????
+    "r0" : (a) => {return Math.round(a)} ,  ///////////////////////???
 };
-
-
-
-
-// 
-
-
 
 
 
@@ -46,6 +50,17 @@ let operatorToFunction = {
 
 
 // KHU VỰC OK RỒI 
+round_0.addEventListener("click", () => {
+    screen.textContent = operatorToFunction["r0"](screen.textContent);
+})
+
+round_2.addEventListener("click", () => {
+    screen.textContent = operatorToFunction["r2"](screen.textContent);
+})
+
+plusMinus.addEventListener("click", () => {
+    screen.textContent = operatorToFunction["plusMinus"](screen.textContent);
+})
 
 square.addEventListener("click", () => {
     //tính toán xong hiển thị lên màn hình 
@@ -69,7 +84,7 @@ pi.addEventListener("click", () => {
 opes.forEach((element, index, arr) => {
     element.addEventListener("click", () => {
         //Khi 1 toán tử được nhấn thì trả về toán hạng đó để tính toán 
-        operator = element.textContent;
+        operators = element.textContent;
     })
 });
 
