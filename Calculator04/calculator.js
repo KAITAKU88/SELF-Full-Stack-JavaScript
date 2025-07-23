3f8ca7d34789f10ba5ad12b4da72c2cff4f567df
khaikaito <210374351+KAITAKU88@users.noreply.github.com>
2025-07-23 08:39:34 +0700

    Do opes.forEach() and qua.addEventListener()

--- Calculator04/calculator.js
+++ Calculator04/calculator.js
@@ -1,1 +1,58 @@
 // nhánh này sẽ phân tích và viết lại logic từ đầu
+let numsBtn = document.querySelectorAll(".number");
+let dot = document.querySelector(".dot");
+let equa = document.querySelector(".equa");
+let operator = "";
+let num1 = 0;
+let num2 = 0; //đây là 2 toán hạng trong phép tính toán 
+
+
+let opes = document.querySelectorAll(".operator");
+
+let forOneOperand = {
+    "&pi;" : Math.PI,
+
+}
+
+//object mapping để chuyển từ operator string sang function 
+let operatorToFunction = {
+    '/' : (a, b) => {return a/b},
+    'x' : (a, b) => {return a*b},
+    '+' : (a, b) => {return a + b},
+    '-' : (a, b) => {return a - b}
+    
+};
+
+
+
+
+// 
+
+
+
+
+
+
+
+
+// KHU VỰC OK RỒI 
+
+
+//Với mỗi nút toán hạng được click thì : 
+opes.forEach((element, index, arr) => {
+    element.addEventListener("click", () => {
+        //Khi 1 toán tử được nhấn thì trả về toán hạng đó để tính toán 
+        operator = element.textContent;
+    })
+});
+
+//khi dấu = được nhấn thì 
+equa.addEventListener("click", () => {
+    //Thực hiện phép tính toán 
+    operatorToFunction[operator](num1, num2);  //vấn đề quan trọng là xác định num1, num2???
+});
+
+
+
+
+
